25ae63263f6a71bacdfa216859c3c85fe32cf995
Jasid Martinez <jasidmartinez21@gmail.com>
2022-03-15 22:30:56 -0500

    Calendar. Missing: years rotate, responsive, details

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -1,155 +1,135 @@
-const actualDay = moment();
+const calendar_container = document.querySelector('.calendar-container');
+const tasks_container = document.querySelector('.calendar-tasks-container');
+const month_names = document.querySelectorAll('.month-name-container');
 
-const firstDay = actualDay.startOf('month');
-const first = get_first_day(firstDay);
-const last = get_last_day(moment(actualDay.endOf('month')));
-create_calendar(first, actualDay, last);
-for(var task of mytasks){
-    const date = moment(task.Date).format('YYYY-MM-DD');
-    add_calendar_task(task, date);
+const curr_month = moment().format('MMMM');
+for(var month of month_names){
+    if(month.getAttribute('data-month').toLowerCase() == curr_month.toLowerCase()){
+        month.classList.add('active');
+    }
 }
+
+
+var first = moment().startOf('month');
+first = moment(first);
+const first_day = get_first_day(first);
+create_calendar(first_day, calendar_container);
+add_tasks(calendar_container, mytasks);
+set_click_event(calendar_container, tasks_container);
+
+month_click_event(month_names, calendar_container, tasks_container);
+
+
 
 
 
 
 //FUNCTIONS.
-function get_first_day(firstDay){
-    var day;
-    var moment_day = moment(firstDay);
-    for(var i=1; i<8; i++){
-        var new_day = moment_day.subtract(i, 'days')
-        var string_new_day = new_day.format('dddd');
-        string_new_day = string_new_day.toLowerCase();
-        if(string_new_day == "monday"){
-            day = new_day;
-            break;
-        }
+function get_first_day(first){  //Must be in moment format.
+    var checker = first;
+    while(checker.format('dddd').toLowerCase() != 'monday'){
+        checker = checker.subtract(1, 'days');
+        checker = moment(checker);
     }
-    return day;
+    return checker;
 }
 
-function get_last_day(last_day){
-    var day;
-    var moment_day = moment(last_day);
-    for(var i=1; i<8; i++){
-        var new_day = moment_day.add(i, 'days')
-        var string_new_day = new_day.format('dddd');
-        string_new_day = string_new_day.toLowerCase();
-        if(string_new_day == "sunday"){
-            day = new_day;
-            break;
+function create_calendar(first, container){
+    if(container){
+        delete_boxes(container);
+        var first_day = first;
+        for(var i=0; i<42; i++){
+            const calendar_box = document.createElement('div');
+            calendar_box.classList.add("calendar-box", "day-value-box");
+            calendar_box.setAttribute('data-date', first_day.format('YYYY-MM-DD'));
+        
+                const day_circle = document.createElement('div');
+                day_circle.classList.add('day-circle');
+                day_circle.innerHTML = first_day.format('DD');
+            calendar_box.appendChild(day_circle);
+        
+            container.appendChild(calendar_box);
+            first_day = first_day.add(1, 'day');
+            first_day = moment(first_day);
         }
+    }else{
+        console.error("Missing calendar container for add calendar boxes");
     }
-    return day;
 }
 
-function create_calendar(first, actual, final){
-    const calendar = document.querySelector(".days-box-zone");
-    var date = first;
-    const difference = final.diff(first, 'days');
-
-    var weeks = new Array();
-    for(var i=0; i<difference/7; i++){
-        const week = document.createElement('div');
-        week.classList.add('calendar-week');
-        week.classList.add('container-fluid');
-
-        weeks.push(week);
+function add_tasks(container, tasks){
+    const boxes = container.querySelectorAll('.day-value-box');
+    for(var box of boxes){
+        var count = 0;
+        for(var task of tasks){
+            const task_date = moment(task.Date).format('YYYY-MM-DD');
+            if(box.getAttribute('data-date') == task_date){
+                count++;
+            }
+        }
+        if(count > 0){
+            const color = setTaskColor(box.getAttribute('data-date'));
+            const task_count = document.createElement('div');
+            task_count.classList.add('task-count');
+            task_count.innerHTML = count;
+            task_count.style.color = color;
+            task_count.style.borderColor = color;
+            box.appendChild(task_count);
+        }
     }
+}
 
-    var cont = 0;
-    for(var i=0; i<=difference; i++){
-        const box = document.createElement('div');
-        box.classList.add('calendar-box');
-        box.classList.add('calendar-general-box');
-        box.setAttribute('id', date.format('YYYY-MM-DD'));
-            const num_circle = document.createElement('div');
-            num_circle.classList.add('num-cirlce');
-            num_circle.innerHTML = date.format('DD');
+function delete_boxes(container){
+    const boxes = container.querySelectorAll('.day-value-box');
+    for(var box of boxes){
+        box.remove();
+    }
+}
 
-            const day_tasks_container = document.createElement('div');
-            day_tasks_container.classList.add('day-tasks-container');
-            day_tasks_container.classList.add('container-fluid');
-
-                const tasks_counter = document.createElement('div');
-                tasks_counter.classList.add('task-counter');
-                var counter = 0;
-                for(var task of mytasks){
-                    if(moment(task.Date).format('YYYY-MM-DD') == date.format('YYYY-MM-DD')){
-                        counter++;
-                    }
+function set_click_event(container, tasks_container){
+    const boxes = container.querySelectorAll('.day-value-box');
+    for(var box of boxes){
+        box.addEventListener('click', function(e){
+            e.preventDefault();
+            tasks_container.innerHTML = null;
+            const box_date = this.getAttribute('data-date');
+            var tasks_array = new Array();
+            for(var task of mytasks){
+                const task_date = moment(task.Date).format('YYYY-MM-DD');
+                if(box_date == task_date){
+                    const disp_task_container = document.createElement('div');
+                    disp_task_container.classList.add('displayed-task-container', 'container');
+                        const disp_task = document.createElement('p');
+                        disp_task.classList.add('displayed-task', 'h5');
+                        disp_task.innerHTML = task.Name + `<span class="disp-task-imp"> (${task.Priority})</span>`;
+                    disp_task_container.appendChild(disp_task);
+                    tasks_container.appendChild(disp_task_container);
                 }
-                if(counter == 0){
-                    tasks_counter.style.display = "none";
-                }
-                tasks_counter.innerHTML = counter;
-            day_tasks_container.appendChild(tasks_counter);
-
-        box.appendChild(num_circle);
-        box.appendChild(day_tasks_container);
-
-        weeks[cont].appendChild(box);
-
-        if(date.format('dddd').toLowerCase() == 'sunday'){
-            cont++;
-        }
-        date = date.add(1, 'days');
-        date = moment(date);
-    }
-
-    for(var week of weeks){
-        calendar.appendChild(week);
+            }
+        });
     }
 }
 
-function add_calendar_task(task, day){
-    const day_box = document.getElementById(day);
-    if(day_box){
-        const tasks_box_container = day_box.querySelector('.day-tasks-container');
-        const color = set_calendar_color(task);
+function month_click_event(month_names, container, tasks_container){
+    for(var month of month_names){
+        month.addEventListener('click', function(e){
+            e.preventDefault();
 
-        const task_box = document.createElement('div');
-        task_box.classList.add('task-box');
-        task_box.classList.add('container');
-        task_box.setAttribute('data-color2', color[1]);
-        task_box.style.border = `2px solid ${color[0]}`;
-        task_box.style.color = color[0];
-        task_box.innerHTML = task.Name;
+            for(var mt of month_names){
+                mt.classList.remove('active');
+            }
+            this.classList.add('active');
 
-        task_box.addEventListener('mousemove', function(e){
-            const color2 = this.getAttribute('data-color2');
-            this.style.backgroundColor = color2;
-            this.style.cursor = 'pointer';
-            this.style.fontWeight = "600";
+            const year = moment().format('YYYY');
+            const month = this.getAttribute('data-month');
+            const date = year + "-" + month;
+            var first = moment(date).startOf('month');
+            first = moment(first);
+
+            const first_day = get_first_day(first);
+            create_calendar(first_day, container);
+            add_tasks(container, mytasks);
+            set_click_event(container, tasks_container);
         });
-
-        task_box.addEventListener('mouseleave', function(e){
-            const color2 = this.getAttribute('data-color2');
-            this.style.backgroundColor = 'transparent';
-            this.style.cursor = 'default';
-            this.style.fontWeight = "400";
-        });
-
-        tasks_box_container.appendChild(task_box);
-    }
-}
-
-function set_calendar_color(task){
-    const imp = task.Priority;
-
-    if(imp.toLowerCase() == 'relax'){
-        return ["#18BFB3", "#AEE6E5"];
-    }
-
-    if(imp.toLowerCase() == 'normal'){
-        return ["#1846BF", "#ADB8E5"];
-    }
-
-    if(imp.toLowerCase() == 'important'){
-        return ["#8518BF", "#D1ADE5"];
-    }
-
-    if(imp.toLowerCase() == 'very'){
-        return ["#BF183B", "#E6AEAE"];
     }
 }
